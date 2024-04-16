940cc2821efff94ca1a9600735e54b47d3fff36e
Dominique Haroun <haroundominique@gmail.com>
2024-04-16 22:54:16 +0200

    frontend done, it should work, but it doesn't fsr

--- demo/src/main/java/com/example/demo/interfaces/IPersona.java
+++ demo/src/main/java/com/example/demo/interfaces/IPersona.java
@@ -2,6 +2,8 @@ package com.example.demo.interfaces;
 
 import com.example.demo.model.Persona;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IPersona extends CrudRepository<Persona, Integer> {
 }
--- demo/src/main/java/com/example/demo/service/PersonaService.java
+++ demo/src/main/java/com/example/demo/service/PersonaService.java
@@ -4,10 +4,12 @@ import com.example.demo.interfaceService.IPersonaService;
 import com.example.demo.interfaces.IPersona;
 import com.example.demo.model.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class PersonaService implements IPersonaService {
     @Autowired
     private IPersona data;
