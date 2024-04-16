7710897bf0efc69b613d9dfe307e2fcc41080f07
Dominique Haroun <haroundominique@gmail.com>
2024-04-16 21:21:08 +0200

    controller done, backend done for the example

--- demo/src/main/java/com/example/demo/controller/Controller.java
+++ demo/src/main/java/com/example/demo/controller/Controller.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
 import com.example.demo.interfaceService.IPersonaService;
+import com.example.demo.model.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 @RequestMapping
@@ -11,8 +15,10 @@ public class Controller {
 
     @Autowired
     private IPersonaService service;
+    @GetMapping("/listar")
     public String listar(Model model){
-        service.listar();
+        List<Persona> personas = service.listar();
+        model.addAttribute("personas", personas);
         return "index";
     }
 }
