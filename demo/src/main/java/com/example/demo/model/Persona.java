a8a6e8a686ae75b608027be1cae826c20eb42040
Dominique Haroun <haroundominique@gmail.com>
2024-04-16 21:06:10 +0200

    Persona class done, IPersona WIP

--- demo/src/main/java/com/example/demo/model/Persona.java
+++ demo/src/main/java/com/example/demo/model/Persona.java
@@ -1,6 +1,12 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "persona")
 public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String phone;
